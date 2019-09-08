7ee13b08510ddad8d6b44deaef5050087f0d794a
hasav <vlj.hasa@gmail.com>
2019-09-08 14:33:04 +0300

    added rest tests, requirements installed, server running

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('hello world')
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://fullstack:moi123@cluster0-q7znc.mongodb.net/blog-app?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+app.get('/info', (request, response) => {
+    response.status(200).send({ infomessage: 'this is backend of blog app' })
+})
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
