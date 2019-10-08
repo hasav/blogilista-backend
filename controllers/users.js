5fa7f424b9fb780107eac7f44b778f0663150443
hasav <vlj.hasa@gmail.com>
2019-10-08 20:11:52 +0300

    4.17 done, added user - blog relation and population functionality

--- controllers/users.js
+++ controllers/users.js
@@ -3,14 +3,15 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+        .find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1})
     response.json(users.map(u => u.toJSON()))
 })
 
 usersRouter.post('/', async (request, response, next) => {
 
     const body = request.body
-    
+
     if (body.password === undefined || body.password.length < 3) {
         return response.status(400).json({ error: 'password missing or too short'})
     }
