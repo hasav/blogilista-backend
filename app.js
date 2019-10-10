1314d9a1b1649b7ab94e2da9520cde71693b17b3
hasav <vlj.hasa@gmail.com>
2019-10-10 10:01:48 +0300

    4.19 and 4.20 done, added token based validation to blog posts, and added middleware to extract token from request

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -11,19 +12,26 @@ blogsRouter.get('/info', (request, response) => {
     response.status(200).send({ infomessage: 'this is database api of blog app' })
 })
 blogsRouter.post('/', async (request, response, next) => {
+    const token = request.token
     const body = request.body 
 
-    const user = await User.findById('5d9cb3ed4ad59e0b044aca5a')
-
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes || 0,
-        user: user._id
-    })
-
     try {
+
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({error: 'token missing'})
+        }
+
+        const user = await User.findById(decodedToken.id)
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes || 0,
+            user: user._id
+        })
+
         const savedBlog = await blog.save()
         user.blogs = user.blogs.concat(savedBlog._id)
         await user.save()
